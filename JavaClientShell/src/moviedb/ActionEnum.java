d8f1159d599ab5bf58d3bfbd88799d3bf59dd3ff
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-02-28 00:39:43 -0500

    adding Java Client command line interface

---
+++ JavaClientShell/src/moviedb/ActionEnum.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package moviedb;
+
+/**
+ * The different actions that can be taken when interacting with the database
+ * @author Colin
+ */
+public enum ActionEnum {
+    ADD, DELETE
+}
---
+++ JavaClientShell/src/moviedb/Storage.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package moviedb;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Colin
+ */
+public class Storage {
+    private final ArrayList<Movie> movieArr;
+    
+    /**
+     * Acts as a middle man for the ArrayList of movies that we have,
+     * allowing for a simpler interface between the rest of the program and the
+     * list of movies
+     */
+    public Storage() {
+        movieArr = new ArrayList<>();
+    }
+    
+    /**
+     * Performs the given action on the list of movies with the provided movies
+     * @param action ActionEnum.ADD|ActionEnum.DELETE, the update action to 
+     * perform either deleting Movies from the database or adding movie to it
+     * @param inArr The array of movies to modify the database with
+     */
+    public void update(ActionEnum action, ArrayList<Movie> inArr) {
+        switch (action) {
+            case ADD:
+                movieArr.addAll(inArr);
+                break;
+            case DELETE:
+                movieArr.removeAll(inArr);
+                break;
+        }
+    }
+    
+    /**
+     * 
+     * @return the database of movies that we have
+     */
+    public ArrayList<Movie> retrieve() {
+        return movieArr;
+    }
+            
+}
