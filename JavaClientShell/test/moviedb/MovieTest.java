5cf13715dbd5f4912b15ea6f2a87561ee27c02fa
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-03-01 22:25:25 -0500

    added testclass for Java Client

---
+++ JavaClientShell/test/moviedb/MovieTest.java
@@ -0,0 +1,27 @@
+package moviedb;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MovieTest {
+
+	@Test
+	public void testGetTitle() {
+		System.out.println("Starting test GetTitle");
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetYear() {
+		System.out.println("Starting test GetYear");
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGenre() {
+		System.out.println("Starting test GetGenre");
+		fail("Not yet implemented");
+	}
+
+}
