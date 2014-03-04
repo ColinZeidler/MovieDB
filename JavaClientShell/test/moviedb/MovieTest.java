bca37399e14a23d7fddcd2e1049cfc4049302178
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-03-03 20:07:53 -0500

    filled out MovieTest for JavaClient

--- JavaClientShell/test/moviedb/MovieTest.java
+++ JavaClientShell/test/moviedb/MovieTest.java
@@ -6,22 +6,37 @@ import org.junit.Test;
 
 public class MovieTest {
 
-	@Test
-	public void testGetTitle() {
-		System.out.println("Starting test GetTitle");
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetTitle() {
+        System.out.println("Starting test: getTitle");
+        String expected = "A new Hope";
+        Movie testM = new Movie(expected, 1000, GenreEnum.SF);
 
-	@Test
-	public void testGetYear() {
-		System.out.println("Starting test GetYear");
-		fail("Not yet implemented");
-	}
+        String result = testM.getTitle();
 
-	@Test
-	public void testGetGenre() {
-		System.out.println("Starting test GetGenre");
-		fail("Not yet implemented");
-	}
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testGetYear() {
+        System.out.println("Starting test: getYear");
+        int expected = 2023;
+        Movie testM = new Movie("title", expected, GenreEnum.SF);
+
+        int result = testM.getYear();
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testGetGenre() {
+        System.out.println("Starting test: getGenre");
+        GenreEnum expected = GenreEnum.SF;
+        Movie testM = new Movie("title", 1000, expected);
+
+        GenreEnum result = testM.getGenre();
+
+        assertEquals(expected, result);
+    }
 
 }
